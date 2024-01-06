ad146dadb8fc7f49507471f4b1dcb4ca1408ec9e
Hamit Özdemir <12991847+hamitozdemir@users.noreply.github.com>
2024-01-06 18:20:13 +0300

    Create polybar launch.sh
    Monitor names modified from previous copy

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload main &
+#   done
+# else
+#   polybar --reload main &
+# fi
+
+MONITOR=DP-0 polybar --reload main &
+MONITOR=HDMI-0 polybar --reload secondary &
+
+# Launch bar1 and bar2
+# polybar example &
+
+echo "Bars launched..."
